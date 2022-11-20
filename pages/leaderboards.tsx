ab384563a36a3f139e96d32dd4c5b22f74b8c63f
erhanb <67612501+sabancihan@users.noreply.github.com>
2022-11-20 15:46:37 +0300

    revert leaderboard

--- pages/leaderboards.tsx
+++ pages/leaderboards.tsx
@@ -1,16 +1,8 @@
-import { useSession, signIn, signOut } from "next-auth/react"
-import { Button } from "@chakra-ui/react";
+
 import Head from "next/head";
-import Image from "next/image";
 import React from "react";
-import styles from "../styles/Home.module.css";
-import Link from "next/link";
-import HomeCompIndex from "../components/home/ui";
-import Navbar from "../components/layout/navbar/navbar";
-import UserCompIndex from "../components/user/ui";
+
 import LBCompIndex from "../components/leaderboard/ui";
-import axios from "axios";
-import { graphqlClient } from "../requests/AxiosInterceptor";
 import { InferGetStaticPropsType } from "next";
 
 type MarketValueRanking = {
@@ -40,9 +32,9 @@ type GraphQLResponse<T>= {
 
 
 
-export default function Home({marketValueRanking,ratingRanking} : InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Home() : InferGetStaticPropsType<typeof getStaticProps> {
 
-  console.log(marketValueRanking,ratingRanking);
+
   
   return (
     <div>
@@ -62,42 +54,26 @@ export default function Home({marketValueRanking,ratingRanking} : InferGetStatic
 }
 
 export const getStaticProps = async () => {
-  console.log("getStaticProps");
-  const marketValueRankings =       await graphqlClient.post<GraphQLResponse<MarketValueRanking>>("", JSON.stringify({
-    query: `
-    query {
-      players {
-        name
-        market_value
-      }
-    }
-    `,
 
-  }))
+  
 
-  const ratingRankings =       await graphqlClient.post<GraphQLResponse<RatingRanking>>("", JSON.stringify({
-    query: `
-    query {
-      players {
-        name
-        rating
-      }
-    }
-    `,
-  }))
+  
+  
+
+
 
 
   return {
     props: {
-      marketValueRanking: marketValueRankings.data,
-      ratingRanking: ratingRankings.data.data.players
+     
 
 
      
       
       
-    },
-    revalidate: 600,
+
+  
     
-  };
+    }
+  }
 }
