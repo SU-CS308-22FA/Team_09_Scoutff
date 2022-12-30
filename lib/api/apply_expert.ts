36d280005e6d242c56d96c3e68eabf272e567df9
ekinnalbantoglu <ekinn@sabanciuniv.edu>
2022-12-30 23:37:33 +0300

    Create Model for Expert Application
    A model similar to Comments has been implemented to match the needs of Expert Application Page.

---
+++ lib/api/apply_expert.ts
@@ -0,0 +1,23 @@
+import mongooseConnection from "../mongoose"
+import Applyexpert, { IApplyexpert, IApplyexpert2 } from "../../models/Applyexpert";
+import console from "console";
+
+export async function postApplyexpert(application : IApplyexpert): Promise<IApplyexpert2>
+{
+    await mongooseConnection();
+    
+    const newApplication = await Applyexpert.create(application);
+
+    return newApplication;
+
+}
+
+export async function getApplyexpert(): Promise<IApplyexpert[]> {
+
+    await mongooseConnection();
+
+    const applyexperts = await Applyexpert.find().lean();
+
+    return applyexperts;
+
+  }
